40e8837c475176f6540adff85f4c59c509f56e25
nicole.ylin0803@gmail.com <nicole.ylin0803@gmail.com>
2023-02-19 22:12:38 -0500

    checking in code for Hello World Project

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by 叶林 on 2/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbExample: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("I am inside viewDidLoad() function")
+    }
+
+    @IBAction func pressMeAction(_ sender: UIButton) {
+        print("Button was pressed")
+        lbExample.text = "Button was pressed"
+    }
+}
+
